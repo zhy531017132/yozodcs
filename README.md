8d7c93427918578ecd6bba032510a6729e23ba26
zhy531017132 <79294762+zhy531017132@users.noreply.github.com>
2021-07-21 16:23:33 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ## yozodcs是什么？
 
-一个dcs新预览调用接口的插件。
+dcs安全预览预览调用接口的插件。
 
-### NPM
+### NPM 暂时未测试，勿用
 
 ```
 npm install yozodcs
@@ -12,21 +12,36 @@ or
 
 
 ```
-<script src="./yozodcs.js"></script>
+<script src="./yozodcs.min.js"></script>
 ```
 
 
 ### 使用说明
-引用canvas的预览通常有两种方式，一种是通过iframe引用，一种将html模板等静态资源一起打包。前者引用方便，后者方便二次开发。
+引用canvas的预览,通过iframe引用，
 
-一、通过iframe引用的情况，传入iframe标签的id即可
+一、通过iframe引用的情况，传入iframe标签的id和跨域文件类型 (type必须传值)
 
 ```
-var render = new DcsRender('iframeId')
+var render = new DcsRender('type','iframeId') 
+
+/*类型说明
+type='pgindex' //ppt
+type='index'  // word
+type='sindex'  // excel
+*/
+
 ```
-二、模板二次开发，无需传入任何参数
+二、同域情况下预览直接调用接口(type必须传值)	
+
 ```
-var render = new DcsRender()
+var render = new DcsRender('type') 
+
+/*类型说明
+type='pgindex' //ppt
+type='index'  // word
+type='sindex'  // excel
+*/
+
 ```
 
 ### 接口使用说明
@@ -41,7 +56,7 @@ lastPage|null|underfined|跳转到上一页|render.lastPage()
 lastPageSync|fn|underfined|跳转到上一页|render.lastPageSync(fn)
 gotoPage|number|underfined|跳转到指定页|render.gotoPage(2)
 gotoPageSync|number,fn|underfined|跳转到指定页|render.gotoPageSync(2,fn)
-getCurrentPage|null|number|返回当前页码|render.getCurrentPage()
+getCurrentPage|null|promise|返回当前页码|render.getCurrentPage()
 
 #### 动画跳转
 
@@ -49,11 +64,31 @@ getCurrentPage|null|number|返回当前页码|render.getCurrentPage()
 --|:--:|--:|--:|--:
 nextAnimation|null|underfined|跳转到下一动画|render.nextAnimation()
 nextAnimationSync|fn|underfined|跳转到下一动画|render.nextAnimationSync(fn)
-preAnimation|null|underfined|跳转到上一动画|render.lastPage()
-preAnimationSync|fn|underfined|跳转到上一动画|render.lastPageSync(fn)
+preAnimation|null|underfined|跳转到上一动画|render.preAnimation()
+preAnimationSync|fn|underfined|跳转到上一动画|render.preAnimationSync(fn)
 gotoAnimation|object1|underfined|跳转到指定页|render.gotoAnimation(object)
-gotoAnimationSync|object1,fn|underfined|跳转到指定页|render.gotoPageSync(object1,fn)
-getAnimationInfo|null|object2|返回当前动画数据|render.getAnimationInfo()
+gotoAnimationSync|object1,fn|underfined|跳转到指定页|render.gotoAnimationSync(object1,fn)
+getAnimationInfo|null|promise|返回当前动画数据|render.getAnimationInfo()
+play|null|underfined|播放|render.play()
+
+#### 缩放
+
+参数|传参|返回值|描述|示例
+--|:--:|--:|--:|--:
+magnify|null|underfined|放大|render.magnify()
+magnifySync|fn|underfined|放大|render.magnifySync(fn)
+shrink|null|underfined|缩小|render.shrink()
+shrinkSync|fn|underfined|缩小|render.shrinkSync(fn)
+
+#### 其他
+
+参数|传参|返回值|描述|示例
+--|:--:|--:|--:|--:
+isHideSidebar|boolean|underfined|显示隐藏左缩略图|render.isHideSidebar(true)
+rotate|boolean|underfined|旋转|render.rotate(true)
+isShowTool|boolean|underfined|显示隐藏工具栏|render.isShowTool(true)
+
+
 
 ##### 注释
 object1：
@@ -62,7 +97,7 @@ object1：
 {
     'pageIndex':number,
     'animationId':number,
-    'type':'next'/'pre
+    'type':'next'/'pre'
 }
 ```
 object2：
@@ -71,12 +106,52 @@ object2：
 {
     'currentAnimIndex':number,
     'page':number,
-    'type':'next'/'pre
+    'type':'next'/'pre'
 }
+```
+#####  demo示例
+跨域请求html：
+```
+<script src="./yozodcs.min.js"></script>
+ <script>
+        var red
+        window.onload = function () {
+            red = new DcsRender('pgindex', 'cont');
+        }
+        //下一页
+        function nextPage() {
+            red.nextPage();
+        }
+         //上一页
+        function prePage() {
+            red.lastPage();
+        }
+        //获取当前页面
+        function getCurrentPage() {
+            //方法一
+            red.getCurrentPage().then(data =>{
+                console.log(data,'当前页面');
+            })
+
+            //方法二
+             async function getCurrentPage() {
+               //return await red.getCurrentPage();
+               var value= await red.getCurrentPage();
+               console.log(value)
+            }
+            getCurrentPage()
+        }
+        //
+        function getAnimationInfo(data) {
+        red.getAnimationInfo().then(data =>{
+            console.log(data,'当前动画信息');
+        })
+    }
+
+       
+ </script>
 ```
 
 ### 有问题反馈
-在使用中有任何问题，欢迎反馈给我，可以用以下联系方式跟我交流。
-#### 联系方式：18018300481
-#### 姓名： 周红雨
+在使用中有任何问题，欢迎反馈。
 
--- yozodcs.js
+++ yozodcs.js
@@ -2,37 +2,72 @@
  * yozodcs v0.1.0
  * By zhouhongyu
  */
+
 ; (function () {
-    var DcsRender = function (iframeId) {
+    var callbackCache;
+    var srcType;
+    var iframe
+    var DcsRender = function (type, iframeId) {
+        if (!type) return
+        srcType = type
+        iframe = iframeId
         this.isCrossDomain = false;
         if (iframeId) {
             this.customerIframe = document.getElementById(iframeId);
             try {
                 this.renderWindow = this.customerIframe.window || this.customerIframe.contentWindow;
-                this.render = this.renderWindow.reader;
+                if (srcType == 'pgindex') {
+                    this.render = this.renderWindow.reader;
+                } else if (srcType == 'index') {
+                    this.render = this.renderWindow.wp;
+                } else if (srcType == 'sindex') {
+                    this.render = this.renderWindow.ss;
+                }
             } catch (err) {
                 this.isCrossDomain = true
                 addEventListener('message', function (event) {
-                    // console.log(event.source.reader.changePageLast);
-                    // console.log(event.source);
+                    var reqType = event.data.reqType
+                    switch (reqType) {
+                        case 'getCurrentPage':
+                            if (!callbackCache) break;
+                            callbackCache(event.data.currentPage)
+                            break;
+                        case 'getAnimationInfo':
+                            if (!callbackCache) break;
+                            object2 = {
+                                currentAnimIndex: event.data.currentAnimIndex,
+                                page: event.data.page,
+                                type: event.data.type
+                            }
+                            callbackCache(object2)
+                            break;
+                        default:
+                            break;
+                    }
                 })
-            }   
+            }
         } else {
-            this.render = window.reader
+            if (srcType == 'pgindex') {
+                this.render = window.reader;
+            } else if (srcType == 'index') {
+                this.render = window.wp;
+            } else if (srcType == 'sindex') {
+                this.render = window.ss;
+            }
         }
     }
     DcsRender.prototype = {
         // 页码跳转
         nextPage: function () {
             if (this.isCrossDomain) {
-                this.customerIframe.contentWindow.postMessage({type:'nextPage',param:''}, '*')
+                this.customerIframe.contentWindow.postMessage({ type: 'nextPage', param: srcType }, '*')
             } else {
                 this.render.changePageNext();
             }
         },
         nextPageSync: function (callback) {
             if (this.isCrossDomain) {
-                this.customerIframe.contentWindow.postMessage({type:'nextPage',param:''}, '*');
+                this.customerIframe.contentWindow.postMessage({ type: 'nextPage', param: srcType }, '*');
                 callback.bind(this)();
             } else {
                 this.render.changePageNext();
@@ -41,14 +76,14 @@
         },
         lastPage: function () {
             if (this.isCrossDomain) {
-                this.customerIframe.contentWindow.postMessage({type:'lastPage',param:''}, '*');
+                this.customerIframe.contentWindow.postMessage({ type: 'lastPage', param: srcType }, '*');
             } else {
                 this.render.changePageLast();
             }
         },
         lastPageSync: function (callback) {
             if (this.isCrossDomain) {
-                this.customerIframe.contentWindow.postMessage({type:'lastPage',param:''}, '*');
+                this.customerIframe.contentWindow.postMessage({ type: 'lastPage', param: srcType }, '*');
                 callback.bind(this)();
             } else {
                 this.render.changePageLast();
@@ -57,14 +92,18 @@
         },
         gotoPage: function (index) {
             if (this.isCrossDomain) {
-                this.customerIframe.contentWindow.postMessage({type:'gotoPage',param:index}, '*');
+                var obj = {
+                    index: index,
+                    srcType: srcType
+                }
+                this.customerIframe.contentWindow.postMessage({ type: 'gotoPage', param: obj }, '*');
             } else {
                 this.render.changePage(index);
             }
         },
         gotoPageSync: function (index, callback) {
             if (this.isCrossDomain) {
-                this.customerIframe.contentWindow.postMessage({type:'gotoPage',param:index}, '*');
+                this.customerIframe.contentWindow.postMessage({ type: 'gotoPage', param: obj }, '*');
                 callback.bind(this)();
             } else {
                 this.render.changePage(index);
@@ -73,22 +112,30 @@
         },
         getCurrentPage: function () {
             if (this.isCrossDomain) {
-                this.customerIframe.contentWindow.postMessage({type:'getCurrentPage',param:''}, '*');
+                return new Promise((resolve, reject) => {
+                    const callback = function (data) {
+                        resolve(data)
+                    }
+                    this.pushCallback(callback);
+                    this.customerIframe.contentWindow.postMessage({ type: 'getCurrentPage', param: srcType }, '*');
+                })
             } else {
-                return this.render.startPageIndex + 1;
+                return new Promise((resolve, reject) => {
+                    resolve(this.render.startPageIndex + 1)
+                })
             }
         },
         // 动画跳转
         nextAnimation: function () {
             if (this.isCrossDomain) {
-                this.customerIframe.contentWindow.postMessage({type:'nextAnimation',param:''}, '*');
+                this.customerIframe.contentWindow.postMessage({ type: 'nextAnimation', param: '' }, '*');
             } else {
                 this.render.animationManager.next();
             }
         },
         nextAnimationSync: function () {
             if (this.isCrossDomain) {
-                this.customerIframe.contentWindow.postMessage({type:'nextAnimation',param:''}, '*');
+                this.customerIframe.contentWindow.postMessage({ type: 'nextAnimation', param: '' }, '*');
                 callback.bind(this)();
             } else {
                 this.render.animationManager.next();
@@ -97,14 +144,14 @@
         },
         preAnimation: function () {
             if (this.isCrossDomain) {
-                this.customerIframe.contentWindow.postMessage({type:'preAnimation',param:''}, '*');
+                this.customerIframe.contentWindow.postMessage({ type: 'preAnimation', param: '' }, '*');
             } else {
                 this.render.animationManager.pre()
             }
         },
         preAnimationSync: function () {
             if (this.isCrossDomain) {
-                this.customerIframe.contentWindow.postMessage({type:'preAnimation',param:''}, '*');
+                this.customerIframe.contentWindow.postMessage({ type: 'preAnimation', param: '' }, '*');
                 callback.bind(this)();
             } else {
                 this.render.animationManager.pre();
@@ -113,14 +160,14 @@
         },
         gotoAnimation: function (animateObj) {
             if (this.isCrossDomain) {
-                this.customerIframe.contentWindow.postMessage({type:'gotoAnimation',param:animateObj}, '*');
+                this.customerIframe.contentWindow.postMessage({ type: 'gotoAnimation', param: animateObj }, '*');
             } else {
                 this.render.animationManager.gotoAnimation(animateObj);
             }
         },
         gotoAnimationSync: function (animateObj) {
             if (this.isCrossDomain) {
-                this.customerIframe.contentWindow.postMessage({type:'gotoAnimation',param:animateObj}, '*');
+                this.customerIframe.contentWindow.postMessage({ type: 'gotoAnimation', param: animateObj }, '*');
                 callback.bind(this)();
             } else {
                 this.render.animationManager.gotoAnimation(animateObj);
@@ -129,15 +176,256 @@
         },
         getAnimationInfo: function () {
             if (this.isCrossDomain) {
-                this.customerIframe.contentWindow.postMessage({type:'getAnimationInfo',param:''}, '*');
+                return new Promise((resolve, reject) => {
+                    const callback = function (data) {
+                        resolve(data)
+                    }
+                    this.pushCallback(callback);
+                    this.customerIframe.contentWindow.postMessage({ type: 'getAnimationInfo', param: srcType }, '*');
+                })
             } else {
-                return {
-                    currentAnimIndex: this.render.animationManager.currentAnimIndex,
-                    page: this.render.animationManager.gotoAimationPageIndex,
-                    type: this.render.animationManager.gotoAimationType
+                return new Promise((resolve, reject) => {
+                    resolve(object2 = {
+                        currentAnimIndex: this.render.animationManager.currentAnimIndex,
+                        page: this.render.animationManager.gotoAimationPageIndex + 1,
+                        type: this.render.animationManager.gotoAimationType
+                    })
+                })
+            }
+        },
+        //放大
+        magnify: function () {
+            if (this.isCrossDomain) {
+                this.customerIframe.contentWindow.postMessage({ type: 'magnify', param: srcType }, '*');
+            } else {
+                if (srcType == 'pgindex' || srcType == 'index') {
+                    this.render.changeTab(1);
+                } else if (srcType == 'sindex') {
+                    this.render.changeScale(1);
                 }
             }
         },
+        magnifySync: function () {
+            if (this.isCrossDomain) {
+                this.customerIframe.contentWindow.postMessage({ type: 'magnify', param: srcType }, '*');
+                callback.bind(this)();
+            } else {
+                if (srcType == 'pgindex' || srcType == 'index') {
+                    this.render.changeTab(1);
+                } else if (srcType == 'sindex') {
+                    this.render.changeScale(1);
+                }
+                callback.bind(this)();
+            }
+        },
+        //缩小
+        shrink: function () {
+            if (this.isCrossDomain) {
+                this.customerIframe.contentWindow.postMessage({ type: 'shrink', param: srcType }, '*');
+            } else {
+                if (srcType == 'pgindex' || srcType == 'index') {
+                    this.render.changeTab(0);
+                } else if (srcType == 'sindex') {
+                    this.render.changeScale(0);
+                }
+            }
+        },
+        shrinkSync: function () {
+            if (this.isCrossDomain) {
+                this.customerIframe.contentWindow.postMessage({ type: 'shrink', param: srcType }, '*');
+                callback.bind(this)();
+            } else {
+                if (srcType == 'pgindex' || srcType == 'index') {
+                    this.render.changeTab(0);
+                } else if (srcType == 'sindex') {
+                    this.render.changeScale(0);
+                }
+                callback.bind(this)();
+            }
+        },
+        //播放
+        play: function (flag) {
+            flag = false//不自动播放
+            if (this.isCrossDomain) {
+                obj = {
+                    flag: flag,
+                    srcType: srcType,
+                }
+                this.customerIframe.contentWindow.postMessage({ type: 'play', param: obj }, '*');
+            } else {
+                this.render.setFullScreen(flag);
+            }
+        },
+        playSync: function (flag) {
+            flag = false//不自动播放
+            if (this.isCrossDomain) {
+                obj = {
+                    flag: flag,
+                    srcType: srcType,
+                }
+                this.customerIframe.contentWindow.postMessage({ type: 'play', param: obj }, '*');
+                callback.bind(this)();
+            } else {
+                this.render.setFullScreen(flag);
+                callback.bind(this)();
+            }
+        },
+        //搜索
+        search: function (search) {
+            if (this.isCrossDomain) {
+                obj = {
+                    srcType: srcType,
+                    search: search
+                }
+                this.customerIframe.contentWindow.postMessage({ type: 'search', param: obj }, '*');
+            } else {
+                if (srcType == 'index') {
+                    this.render.startSearch(search);
+                } else if (srcType == 'sindex') {
+                    this.render.beginSearch(search, true);
+                }
+            }
+        },
+        searchSync: function () {
+            if (this.isCrossDomain) {
+                this.customerIframe.contentWindow.
+                    obj = {
+                    srcType: srcType,
+                    search: search
+                }
+                postMessage({ type: 'search', param: obj }, '*');
+                callback.bind(this)();
+            } else {
+                if (srcType == 'index') {
+                    this.render.startSearch(search);
+                } else if (srcType == 'sindex') {
+                    this.render.beginSearch(search, true);
+                }
+                callback.bind(this)();
+            }
+        },
+        //顺（逆）时针旋转
+        rotate: function (flag) {
+            if (this.isCrossDomain) {
+                obj = {
+                    flag: flag,
+                    srcType: srcType,
+                }
+                this.customerIframe.contentWindow.postMessage({ type: 'rotate', param: obj }, '*');
+            } else {
+                if (srcType == 'index') {
+                    this.render.rotate(flag);
+                }
+            }
+        },
+        rotateSync: function (flag) {
+            if (this.isCrossDomain) {
+                this.customerIframe.contentWindow.
+                    obj = {
+                    flag: flag,
+                    srcType: srcType,
+                }
+                postMessage({ type: 'rotate', param: obj }, '*');
+                callback.bind(this)();
+            } else {
+                if (srcType == 'index') {
+                    this.render.rotate(flag);
+                }
+                callback.bind(this)();
+            }
+        },
+        //隐藏显示缩略图
+        isHideSidebar: function (flag) {
+            if (this.isCrossDomain) {
+                obj = {
+                    flag: flag,
+                    srcType: srcType,
+                }
+                this.customerIframe.contentWindow.postMessage({ type: 'isHideSidebar', param: obj }, '*');
+            } else {
+                if (srcType == 'pgindex') {
+                    this.render.isHideSidebar(flag);
+                }
+            }
+        },
+        isHideSidebarSync: function (flag) {
+            if (this.isCrossDomain) {
+                obj = {
+                    flag: flag,
+                    srcType: srcType,
+                }
+                this.customerIframe.contentWindow.postMessage({ type: 'isHideSidebar', param: obj }, '*');
+                callback.bind(this)();
+            } else {
+                if (srcType == 'pgindex') {
+                    this.render.isHideSidebar(flag);
+                }
+                callback.bind(this)();
+            }
+        },
+        //隐藏显示工具栏
+        isShowTool: function (flag) {
+            if (this.isCrossDomain) {
+                obj = {
+                    flag: flag,
+                    srcType: srcType,
+                }
+                this.customerIframe.contentWindow.postMessage({ type: 'isShowTool', param: obj }, '*');
+            } else {
+                // $(window.frames["iframeName"].document).find("#title-box").css('display', 'none')
+                if (srcType == 'index' || srcType == 'sindex') {
+                    this.render.isShowTool(flag);
+                }
+            }
+        },
+        isShowToolSync: function (flag) {
+            if (this.isCrossDomain) {
+                obj = {
+                    flag: flag,
+                    srcType: srcType,
+                }
+                this.customerIframe.contentWindow.postMessage({ type: 'isShowTool', param: obj }, '*');
+                callback.bind(this)();
+            } else {
+                if (srcType == 'index' || srcType == 'sindex') {
+                    this.render.isShowTool(flag);
+                }
+                callback.bind(this)();
+            }
+        },
+        //全屏
+        fullScreen: function () {
+            if (this.isCrossDomain) {
+                this.fullScreen1(iframe)
+            } else {
+                this.fullScreen1(iframe)
+            }
+        },
+        fullScreenSync: function () {
+            if (this.isCrossDomain) {
+                this.customerIframe.contentWindow.postMessage({ type: 'fullScreen', param: srcType }, '*');
+                callback.bind(this)();
+            } else {
+
+                callback.bind(this)();
+            }
+        },
+        pushCallback: function (callback) {
+            callbackCache = callback
+        },
+        fullScreen1: function (domId) {
+            var element = document.getElementById(domId);
+            var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen
+                || element.mozRequestFullScreen || element.msRequestFullScreen;
+            if (requestMethod) {
+                requestMethod.call(element);
+            } else if (typeof window.ActiveXObject !== "undefined") {
+                var wscript = new ActiveXObject("WScript.Shell");
+                if (wscript !== null) {
+                    wscript.SendKeys("{Esc}");
+                }
+            }
+        }
     }
 
     if (typeof module !== 'undefined' && typeof exports === 'object') {
